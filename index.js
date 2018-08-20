8daa32862f5f2614c715b1fc90d884d81b2de500
Chistikov Vlad <30637203+Chistikov@users.noreply.github.com>
2018-08-20 17:45:12 +0300

    rewrite on express

--- index.js
+++ index.js
@@ -1,17 +1,27 @@
 var express = require('express');
 var mysql = require('mysql');
 
-var connection = mysql.createConnection({
-	host: 'localhost',
-	user: 'root',
-	password: '',
-	database: 'siteDB'
+// var connection = mysql.createConnection({
+// 	host: 'localhost',
+// 	user: 'root',
+// 	password: '',
+// 	database: 'siteDB'
+// });
+
+// connection.connect(function(){
+// 	if(!error){
+// 		console.log('error');
+// 	} else {
+// 		console.log('All ok!');
+// 	}
+// });
+
+
+
+var app = express();
+
+app.get('/', function(request, response){
+	response.send('all ok');
 });
 
-connection.connect(function(){
-	if(!error){
-		console.log('error');
-	} else {
-		console.log('All ok!');
-	}
-});
+app.listen(3000);
